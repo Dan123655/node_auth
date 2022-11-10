4581d9b597ad87b0c26914e15427e62ae0b9ded8
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2022-11-10 12:51:34 +0100

    schema

--- models/Role.js
+++ models/Role.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose')
 
 const Role = new Schema({
     value: { type: String, unique: true, default: 'USER' },
+    tasks: { type: String, unique: true, default: '[]' },
 
 })
 module.exports = model('Role', Role)
