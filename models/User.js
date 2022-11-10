ba2ada5a8274abf34b2d51fe227ee5bb5b5635b3
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2022-11-10 14:04:28 +0100

    11000

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose')
 const User = new Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    roles: [{ type: String, ref: 'Role' }]
+    roles: [{ type: String, ref: 'Role' }],
+    tasks: [{ type: String,default:'[]',required:false}]
 })
 module.exports = model('User', User)
