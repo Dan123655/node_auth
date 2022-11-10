f7560523721dc5fdf8e8f04b57494af710394b2d
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2022-11-10 15:17:07 +0100

    indexes

--- models/User.js
+++ models/User.js
@@ -4,5 +4,6 @@ const User = new Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     roles: [{ type: String, ref: 'Role' }],
+    tasks:{type:String,unique:false,required:true}
 })
 module.exports = model('User', User)
