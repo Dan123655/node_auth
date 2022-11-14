955817559c939946656b0fadc8d05a486c1fec76
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2022-11-14 23:31:04 +0100

    first?commit

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(cors({
     credentials: true,
     'Access-Control-Allow-Credentials': true,
-    // 'Access-Control-Allow-Origin': 'https://dan123655.github.io',
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
-    origin: 'http://localhost:3000'
-    // origin: 'https://dan123655.github.io'
+    'Access-Control-Allow-Origin': 'https://dan123655.github.io',
+    // 'Access-Control-Allow-Origin': 'http://localhost:3000',
+    // origin: 'http://localhost:3000'
+    origin: 'https://dan123655.github.io'
   }))
 
 app.use(express.json());
