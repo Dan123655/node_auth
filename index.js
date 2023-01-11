ebf1f899131b920cf1df337555db806d37bef831
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2023-01-11 04:38:00 +0100

    secret key

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT||3500;
 const mongoose = require("mongoose");
 const authRouter = require("./authRouter");
-
+import { myKey } from "./key.js";
 const cors = require("cors");
 const app = express();
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use("/api", authRouter);
 const start = async () => {
 
     try {
-        await mongoose.connect(`mongodb+srv://node_user:SFnZZH4eGfuRPMCk@cluster0.d5adm64.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`${myKey}`)
         app.listen(PORT, () => console.log(`server started on ${PORT}`))
     }
     
